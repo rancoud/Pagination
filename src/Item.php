9e27b7de06b8784d8384b22beb77abee39e3463e
Rancoud <rancoud@users.noreply.github.com>
2025-04-05 22:33:45 +0200

    chore: add better doc (#244)

--- src/Item.php
+++ src/Item.php
@@ -9,15 +9,48 @@ namespace Rancoud\Pagination;
  */
 class Item
 {
+    /**
+     * @var string Aria label
+     */
     public string $ariaLabel = '';
+
+    /**
+     * @var string Href attribute
+     */
     public string $href = '';
+
+    /**
+     * @var string Item HTML attributes
+     */
     public string $itemAttrs = '';
+
+    /**
+     * @var string Link HTML attributes
+     */
     public string $linkAttrs = '';
+
+    /**
+     * @var string Text to display
+     */
     public string $text = '';
 
+    /**
+     * @var bool Is Current
+     */
     public bool $isCurrent = false;
+
+    /**
+     * @var bool Is Dots
+     */
     public bool $isDots = false;
+
+    /**
+     * @var bool Is Disabled
+     */
     public bool $isDisabled = false;
 
+    /**
+     * @var int Page
+     */
     public int $page = 0;
 }
